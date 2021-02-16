84207da957df795e4e139c209eeda265e184011f
nejicadena <nejicadena@gmail.com>
2021-02-16 15:00:34 -0600

    add acerca

--- vecfroms/core/urls.py
+++ vecfroms/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView, SamplePageView, AboutPageView
+from .views import HomePageView, SamplePageView, AboutPageView, AcercaTallerPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
     path('sample/', SamplePageView.as_view(), name="sample"),
     path('about/', AboutPageView.as_view(), name="about"),
+    path('acerca/', AcercaTallerPageView.as_view(), name="acerca"),
 ]
