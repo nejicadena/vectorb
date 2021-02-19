5284c0f1cec79e3144b8b7fcec410dabfab89a5b
nejicadena <nejicadena@gmail.com>
2021-02-18 22:57:27 -0600

    test4

--- vecfroms/core/views.py
+++ vecfroms/core/views.py
@@ -4,9 +4,6 @@ from django.shortcuts import render
 class HomePageView(TemplateView):
     template_name = "core/home.html"
 
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'title':"TALLER EN LÍNEA"})
-
 class AboutPageView(TemplateView):
     template_name = "core/about.html"
     
