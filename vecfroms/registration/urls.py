4c923fdd31a75b1ba491c98300c876b6aab45448
nejicadena <nejicadena@gmail.com>
2021-02-18 22:25:10 -0600

    feature

--- vecfroms/registration/urls.py
+++ vecfroms/registration/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from .views import SignUpView, ConsentPageView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name="signup"),
-    path('consen/', ConsentPageView.as_view(), name="con"),
+    path('consen/', TemplateView.as_view(template_name='registration/consentimiento.html'), name="con"),
     
 ]
