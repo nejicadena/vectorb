5c0c4a5ad9eaf6f58dfc4529528655826fb50b1f
nejicadena <nejicadena@gmail.com>
2021-02-28 19:01:44 -0600

    modules fix

--- vecfroms/modulesv/urls.py
+++ vecfroms/modulesv/urls.py
@@ -1,18 +1,25 @@
 from django.urls import path
-from .views import ModulesPageView, ModulesPageView1, ModulesPageView2, ModulesPageView3, CreateComment1,CreateComment2, CreateComment3 ,LayerModel1,LayerModel2,LayerModel3,ModulesPageView11,ModulesPageView12
+from .views import ModulesPageView, ModulesPageView1, ModulesPageView11,ModulesPageView12
+                    # ModulesPageView2, 
+                    # ModulesPageView3, 
+                    # CreateComment1,
+                    # CreateComment2, 
+                    # CreateComment3 ,
+                    # LayerModel1,LayerModel2,LayerModel3,
+                    
 
 urlpatterns = [
     path('', ModulesPageView.as_view(), name="modules"),
     path('1', ModulesPageView1.as_view(), name="modules1"),
     path('11', ModulesPageView11.as_view(), name="modules11"),
     path('12', ModulesPageView12.as_view(), name="modules12"),
-    path('2', ModulesPageView2.as_view(), name="modules2"),
-    path('3', ModulesPageView3.as_view(), name="modules3"),
-    path('comentario', CreateComment1.as_view(), name="comment1"),
-    path('comentario2', CreateComment2.as_view(), name="comment2"),
-    path('comentario3', CreateComment3.as_view(), name="comment3"),
-    path('1/layer1', LayerModel1.as_view(),name="layer1"),
-    path('2/layer2', LayerModel2.as_view(),name="layer2"),
-    path('3/layer3', LayerModel3.as_view(),name="layer3"),
+    # path('2', ModulesPageView2.as_view(), name="modules2"),
+    # path('3', ModulesPageView3.as_view(), name="modules3"),
+    # path('comentario', CreateComment1.as_view(), name="comment1"),
+    # path('comentario2', CreateComment2.as_view(), name="comment2"),
+    # path('comentario3', CreateComment3.as_view(), name="comment3"),
+    # path('1/layer1', LayerModel1.as_view(),name="layer1"),
+    # path('2/layer2', LayerModel2.as_view(),name="layer2"),
+    # path('3/layer3', LayerModel3.as_view(),name="layer3"),
     
 ]
